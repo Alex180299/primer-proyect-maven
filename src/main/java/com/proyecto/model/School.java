ea90b30aaebb85f03cf61955cbb3402d7949f4dd
Alex180299 <ale_18.99@hotmail.com>
2020-05-08 10:12:03 -0500

    Se agregaron la libreria Jackson y el uso de los streams

---
+++ src/main/java/com/proyecto/model/School.java
@@ -0,0 +1,231 @@
+package com.proyecto.model;
+
+import java.util.List;
+
+public class School {
+    private String name;
+    private String website;
+    private List<String> levels;
+    private String High;
+    private String format;
+    private String format_description;
+    private String gender;
+    private String description;
+    private List<String> languages;
+    private String money_needed;
+    private String online_only;
+    private String number_of_students;
+    private String contact_name;
+    private String contact_number;
+    private String contact_email;
+    private String latitude;
+    private String longitude;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String published;
+    private String updated_at;
+    private String country;
+    private String source;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public List<String> getLevels() {
+        return levels;
+    }
+
+    public void setLevels(List<String> levels) {
+        this.levels = levels;
+    }
+
+    public String getHigh() {
+        return High;
+    }
+
+    public void setHigh(String high) {
+        High = high;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getFormat_description() {
+        return format_description;
+    }
+
+    public void setFormat_description(String format_description) {
+        this.format_description = format_description;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    public String getMoney_needed() {
+        return money_needed;
+    }
+
+    public void setMoney_needed(String money_needed) {
+        this.money_needed = money_needed;
+    }
+
+    public String getOnline_only() {
+        return online_only;
+    }
+
+    public void setOnline_only(String online_only) {
+        this.online_only = online_only;
+    }
+
+    public String getNumber_of_students() {
+        return number_of_students;
+    }
+
+    public void setNumber_of_students(String number_of_students) {
+        this.number_of_students = number_of_students;
+    }
+
+    public String getContact_name() {
+        return contact_name;
+    }
+
+    public void setContact_name(String contact_name) {
+        this.contact_name = contact_name;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getContact_email() {
+        return contact_email;
+    }
+
+    public void setContact_email(String contact_email) {
+        this.contact_email = contact_email;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
